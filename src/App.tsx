a11f76cfde96bf9296e06b8ca07c9fade29bc027
Anna Zanovskaya <zanovskaya@mercurydevelopment.com>
2022-05-27 22:02:42 +0300

    Add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
-import {QueryClient, QueryClientProvider, useQuery} from 'react-query';
+import { QueryClient, QueryClientProvider, useQuery } from 'react-query';
 
 const queryClient = new QueryClient();
 
@@ -9,20 +9,24 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <div className="App">
-        <Pokemons/>
+        <Pokemons />
       </div>
     </QueryClientProvider>
   );
 }
 
 function Pokemons() {
-  const queryPokemon = useQuery('pokemon', () => axios.get('https://pokeapi.co/api/v2/pokemon').then(res => res.data.results))
-  console.log(queryPokemon)
-  return <div>
-    {queryPokemon?.data?.map((pokemon: any) => {
-      return <div key={pokemon.name}>{pokemon.name}</div>
-    })}
-  </div>
+  const queryPokemon = useQuery('pokemon', () =>
+    axios.get('https://pokeapi.co/api/v2/pokemon').then((res) => res.data.results),
+  );
+  console.log(queryPokemon);
+  return (
+    <div>
+      {queryPokemon?.data?.map((pokemon: any) => {
+        return <div key={pokemon.name}>{pokemon.name}</div>;
+      })}
+    </div>
+  );
 }
 
 export default App;
